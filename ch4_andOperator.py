22093d0266538bb7953d69b8a559bdf0254d7e7d
the-nerdy-geek <57044943+the-nerdy-geek@users.noreply.github.com>
2020-03-17 21:55:42 -0400

    Add files via upload

---
+++ ch4_andOperator.py
@@ -0,0 +1,9 @@
+number = int(input("Give a number between 1 and 200. "))
+if number > 99 and number % 2 == 0:
+    print("That's a large, even number.")
+elif number > 99 and number % 2 != 0:
+    print("That's a large, odd number.")
+elif number < 100 and number % 2 == 0:
+    print("That's a small, even number.")
+else:
+    print("That's a small, odd number.")
---
+++ ch4_ifStatement.py
@@ -0,0 +1,3 @@
+number = int(input("What's your favorite number? "))
+if number < 100:
+    print("That's not a very large number.")

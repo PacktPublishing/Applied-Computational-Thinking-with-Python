7f3bd0389c7b5fb22ea54cb68ce329019fa52bfa
the-nerdy-geek <57044943+the-nerdy-geek@users.noreply.github.com>
2020-06-26 13:47:55 -0400

    Add files via upload

---
+++ ch4_Error1.py
@@ -0,0 +1,7 @@
+number = int(input("Type the number of fries you are ordering: "))
+
+cost = 1.50
+
+total = number * number
+
+print("Your total cost is $" + str(total) + ".")
---
+++ ch4_Error1_fixed.py
@@ -0,0 +1,7 @@
+number = int(input("Type the number of fries you are ordering: "))
+
+cost = 1.50
+
+total = cost * number
+
+print("Your total cost is $" + str(total) + ".")

3c7ccca980c715f97ece932cb1ef287e1263b264
the-nerdy-geek <57044943+the-nerdy-geek@users.noreply.github.com>
2020-11-02 11:42:32 -0500

    Update ch12_csvAnalysis_3.py

--- ch12_csvAnalysis_3.py
+++ ch12_csvAnalysis_3.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import os
 
 #Let Python know correct directory for file.
-os.chdir('C:\\Users\\sofia.dejesus\\OneDrive - Hawken School\\03_book\\Program Files\\Chapter 12')
+os.chdir('C:\\...')
 
 graduates = pd.read_csv('ch12_data.csv', index_col = 0)
 print(graduates[0:13])

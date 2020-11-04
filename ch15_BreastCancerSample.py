07032a7a19ce8166a948b95494dbdae0d48f3f6f
the-nerdy-geek <57044943+the-nerdy-geek@users.noreply.github.com>
2020-11-03 19:28:20 -0500

    Update ch15_BreastCancerSample.py

--- ch15_BreastCancerSample.py
+++ ch15_BreastCancerSample.py
@@ -5,7 +5,7 @@ import seaborn as sns
 %matplotlib inline
 
 #Get data set. Remember to check your directory and/or add the full location of the file.
-dataset = pd.read_csv('C:\\...\\Breast_cancer_data.csv')
+dataset = pd.read_csv('C:\\...\\breast-cancer.csv')
 dataset.head()
 dataset.isnull().sum()
 
@@ -15,7 +15,7 @@ count
 
 #Create bargraph of the diagnosis values
 count.plot(kind = 'bar')
-plt.title('Tumor distribution (malignant: 1, benign: 0)')
+plt.title('Tumor distribution (malignant: M, benign: B)')
 plt.xlabel('Diagnosis')
 plt.ylabel('count')
 plt.show()
@@ -24,4 +24,4 @@ dataset.columns.values
 dataset['target'] = dataset['diagnosis'].map({0:'B',1:'M'})
 
 #Create scatterplot of mean perimeter and mean texture
-sns.scatterplot(x = 'mean_perimeter', y = 'mean_texture', data = dataset, hue = 'target', palette = 'prism');
+sns.scatterplot(x = 'perimeter_mean', y = 'texture_mean', data = dataset, hue = 'diagnosis', palette = 'bright');

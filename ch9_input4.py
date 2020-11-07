38dc721d678ec6358f7f7b393bfe3a65c31ce99f
the-nerdy-geek <57044943+the-nerdy-geek@users.noreply.github.com>
2020-11-07 15:51:50 -0500

    Update ch9_input4.py

--- ch9_input4.py
+++ ch9_input4.py
@@ -6,7 +6,7 @@ name = int(input("How many names will be in the list? "))
   
 #Iterate to add each name to the list
 for i in range(0, name): 
-    people = str(input("Type the next name on the list. ")) 
+    people = input("Type the next name on the list. ")
   
     names.append(people) 
       

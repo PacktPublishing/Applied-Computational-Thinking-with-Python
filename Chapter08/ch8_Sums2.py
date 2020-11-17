c3b88d4a3f13b0522123a4c61a08cd9e348c2ab6
the-nerdy-geek <57044943+the-nerdy-geek@users.noreply.github.com>
2020-11-17 12:50:09 -0500

    Update ch8_Sums2.py

--- Chapter08/ch8_Sums2.py
+++ Chapter08/ch8_Sums2.py
@@ -1,4 +1,4 @@
-a = int(input("Provide the first name to be added. "))
+a = int(input("Provide the first number to be added. "))
 b = int(input("Please provide the second number to be added. "))
 c = int(input("Provide the last number to be added. "))
 
